648ef9af9bc3baff32be92bee0fe5f1ffcf9cb57
ligang <ligang1109@aliyun.com>
2017-10-25 09:03:07 +0800

    redis client add cmdLogFmt

--- redis/client.go
+++ redis/client.go
@@ -8,9 +8,12 @@ import (
 	"fmt"
 )
 
+type CmdLogFmtFunc func(cmd string, args ...interface{}) []byte
+
 type Client struct {
 	config *Config
 	logger golog.ILogger
+	clff   CmdLogFmtFunc
 
 	conn redis.Conn
 }
@@ -39,16 +42,27 @@ func NewClient(config *Config, logger golog.ILogger) (*Client, error) {
 		logger = new(golog.NoopLogger)
 	}
 
-	return &Client{
+	this := &Client{
 		config: config,
 		logger: logger,
 
 		conn: conn,
-	}, nil
+	}
+	this.clff = this.cmdLogFmt
+
+	return this, nil
 }
 
-func (this *Client) SetLogger(logger golog.ILogger) {
+func (this *Client) SetLogger(logger golog.ILogger) *Client {
 	this.logger = logger
+
+	return this
+}
+
+func (this *Client) SetCmdLogFmtFunc(clff CmdLogFmtFunc) *Client {
+	this.clff = clff
+
+	return this
 }
 
 func (this *Client) Free() {
@@ -103,11 +117,18 @@ func (this *Client) ExecTrans() ([]*Reply, error) {
 }
 
 func (this *Client) log(cmd string, args ...interface{}) {
+	msg := this.clff(cmd, args...)
+	if msg != nil {
+		this.logger.Log(this.config.LogLevel, msg)
+	}
+}
+
+func (this *Client) cmdLogFmt(cmd string, args ...interface{}) []byte {
 	for _, arg := range args {
 		cmd += " " + fmt.Sprint(arg)
 	}
 
-	this.logger.Log(this.config.LogLevel, []byte(cmd))
+	return []byte(cmd)
 }
 
 func (this *Client) multiDo(cmd string) ([]*Reply, error) {
